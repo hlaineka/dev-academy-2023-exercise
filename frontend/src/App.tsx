47193765366df631d47d4c45c1cb2df046f00129
Helvi <helvi.lainekallio@robu.fi>
2023-01-22 11:59:50 +0200

    TypeScript frontend setup started

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+				hello!
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
