9f78e919eb5c75a67b518991c08394c64887d182
Helvi <helvi.lainekallio@robu.fi>
2023-01-25 16:13:23 +0200

    connected backend to front with Apollo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,32 @@
+import * as React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import MockPage from './components/MockPage';
+import {
+  ApolloClient,
+  ApolloProvider,
+  gql,
+  InMemoryCache,
+} from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { GetAllStationsQuery } from './generated/graphql';
 
-function App() {
+const createApolloClient = () => {
+  return new ApolloClient({
+    uri: 'http://localhost:8080/v1/graphql',
+    cache: new InMemoryCache(),
+  });
+};
+
+const App = () => {
+  const client = createApolloClient();
+
   return (
-    <div>
+    <ApolloProvider client={client}>
       <Routes>
-				hello!
+        <Route path="/" element={<MockPage />} />
       </Routes>
-    </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
